6bc8c303875dca34481a94225aa8e5389277ef25
Victorio Berra <toryberra@gmail.com>
2015-11-18 21:37:38 +0000

    Adapt to new cooldown plugin

--- plugin.js
+++ plugin.js
@@ -14,17 +14,16 @@ var TennuGoogle = {
     requiresRoles: ["admin"],
     init: function(client, imports) {
 
-        const adminCooldown = client._plugins.getRole("admin-cooldown");
+        const adminCooldown = client._plugins.getRole("cooldown");
 
         const requiresAdminHelp = "Requires admin privileges.";
         const googleRequestFailed = 'Failed to fetch results from Google.';
 
         var googleConfig = client.config("agoogle");
-        
+
         // Confirm config values are present
-        if(!googleConfig || !googleConfig.hasOwnProperty('limitResults') || !googleConfig.hasOwnProperty('maxUserDefinedLimit'))
-        {
-            throw Error('tennu-agoogle: is missing some or all of its configuration.');   
+        if (!googleConfig || !googleConfig.hasOwnProperty('limitResults') || !googleConfig.hasOwnProperty('maxUserDefinedLimit')) {
+            throw Error('tennu-agoogle: is missing some or all of its configuration.');
         }
 
         var isAdmin = imports.admin.isAdmin;
@@ -34,10 +33,10 @@ var TennuGoogle = {
                 client._logger.warn('tennu-google: Cooldown plugin found but no cooldown defined.')
             }
             else {
-                isAdmin = adminCooldown.isAdmin;
+                isAdmin = adminCooldown(cooldown);
             }
         }
-        
+
         var limitResults = googleConfig.limitResults;
         var maxUserDefinedLimit = googleConfig.maxUserDefinedLimit;
 
@@ -65,56 +64,62 @@ var TennuGoogle = {
         function handleSearch(IRCMessage) {
             return isAdmin(IRCMessage.hostmask, "google").then(function(isadmin) {
 
-                if (!isadmin) {
-                    client._logger.warn('tennu-agoogle: Unauthorized host `' + IRCMessage.prefix + '` attempted command: `' + IRCMessage.message + '`');
-                    return {
-                        intent: 'notice',
-                        query: true,
-                        message: requiresAdminHelp
-                    };
-                }
-
-                // Its important to note, the default minimist values are set via the config.
-                // Otherwise, changing limitResults would persist.
-                var sayArgs = parseArgs(IRCMessage.args, minimistConfig);
-
-                if (sayArgs.limit) {
-                    var limit = parseInt(sayArgs.limit);
-                    if (limit !== "NaN" && limit > 0 && limit <= maxUserDefinedLimit) {
-                        limitResults = sayArgs.limit;
+                    // isAdmin will be "undefined" if cooldown system is enabled
+                    // isAdmin will be true/false if cooldown system is disabled
+                    if (typeof(isAdmin) !== "undefined" && isAdmin === false) {
+                        throw new Error(requiresAdminHelp);
                     }
-                    else {
+
+                    // Its important to note, the default minimist values are set via the config.
+                    // Otherwise, changing limitResults would persist.
+                    var sayArgs = parseArgs(IRCMessage.args, minimistConfig);
+
+                    if (sayArgs.limit) {
+                        var limit = parseInt(sayArgs.limit);
+                        if (limit !== "NaN" && limit > 0 && limit <= maxUserDefinedLimit) {
+                            limitResults = sayArgs.limit;
+                        }
+                        else {
+                            return {
+                                intent: "notice",
+                                query: true,
+                                message: format('%s is not valid. Please pass in 1-%s.', sayArgs.limit, maxUserDefinedLimit)
+                            };
+                        }
+                    }
+
+                    return google(sayArgs._.join(' '), limitResults).then(function(response) {
+
+                        if (!response.responseData.results.length) {
+                            return 'No results.';
+                        }
+
+                        var results = response.responseData.results.slice(0, limitResults);
+                        return results.map(function(result) {
+                            return format('%s - %s', result.titleNoFormatting, result.unescapedUrl);
+                        });
+
+                    }).catch(function(err) {
+                        client._logger.error(googleRequestFailed);
+                        client._logger.error(err);
                         return {
-                            intent: "notice",
+                            intent: 'notice',
                             query: true,
-                            message: format('%s is not valid. Please pass in 1-%s.', sayArgs.limit, maxUserDefinedLimit)
+                            message: googleRequestFailed
                         };
-                    }
-                }
-
-                return google(sayArgs._.join(' '), limitResults).then(function(response) {
-
-                    if (!response.responseData.results.length) {
-                        return 'No results.';
-                    }
-
-                    var results = response.responseData.results.slice(0, limitResults);
-                    return results.map(function(result) {
-                        return format('%s - %s', result.titleNoFormatting, result.unescapedUrl);
                     });
 
                 })
-
-            }).catch(function(err) {
-                client._logger.error(googleRequestFailed);
-                client._logger.error(err);
-                return {
-                    intent: 'notice',
-                    query: true,
-                    message: googleRequestFailed
-                };
-            });
+                .catch(adminFail);
         };
+
+        function adminFail(err) {
+            return {
+                intent: 'notice',
+                query: true,
+                message: err
+            };
+        }
 
         return {
             handlers: {
